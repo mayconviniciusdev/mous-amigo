655c023392cf287e2eb1cb1e94268e19355dd4a1
Maycon Vinicius <contato@mayconviniciusdev.com>
2025-11-22 13:36:38 -0300

    feat: add CRUD routes for event groups with authentication

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as auth from '../controllers/auth.js';
 import * as events from '../controllers/events.js';
+import * as groups from '../controllers/groups.js';
 
 const router = Router();
 router.post('/login', auth.login);
@@ -11,5 +12,11 @@ router.get('/events/:id', auth.validate, events.getEvent);
 router.post('/events', auth.validate, events.addEvent);
 router.put('/events/:id', auth.validate, events.uptadeEvent);
 router.delete('/events/:id', auth.validate, events.deleteEvent);
+
+router.get('/events/:id_event/groups', auth.validate, groups.getAll);
+router.get('/events/:id_event/groups/:id', auth.validate, groups.getGroup);
+router.post('/events/:id_event/groups', auth.validate, groups.addGroup);
+router.put('/events/:id_event/groups/:id', auth.validate, groups.updateGroup);
+router.delete('/events/:id_event/groups/:id', auth.validate, groups.deleteGroup);
 
 export default router;
